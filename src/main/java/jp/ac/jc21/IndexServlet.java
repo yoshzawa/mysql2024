0972756c59558f681453d5e171c93594f8d9cb2e
yoshzawa <yoshzawa@gmail.com>
2023-05-05 14:45:26 +0900

    TestDBへの接続に成功

--- src/main/java/jp/ac/jc21/IndexServlet.java
+++ src/main/java/jp/ac/jc21/IndexServlet.java
@@ -1,6 +1,9 @@
 package jp.ac.jc21;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,10 +18,27 @@ import javax.servlet.http.HttpServletResponse;
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	final String dbServer = "192.168.100.160";
+	final String dbPort = "3306";
+	final String dbName = "test2023";
+	final String user = "test2023";
+	final String pass = "test2023";
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String url = "jdbc:mysql://"+dbServer+"/"+dbName;
+		response.getWriter().append("Connect to : ").append(url);
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection  conn = DriverManager.getConnection(url, user, pass);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+
 	}
 
 }
