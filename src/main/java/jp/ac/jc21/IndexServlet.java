7f36a933dd329109c97c2e6c45ea6df32a4a814e
yoshzawa <yoshzawa@gmail.com>
2023-05-05 21:37:53 +0900

    一覧表示する

--- src/main/java/jp/ac/jc21/IndexServlet.java
+++ src/main/java/jp/ac/jc21/IndexServlet.java
@@ -3,6 +3,8 @@ package jp.ac.jc21;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
@@ -28,10 +30,26 @@ public class IndexServlet extends HttpServlet {
 			throws ServletException, IOException {
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
 		String url = "jdbc:mysql://"+dbServer+"/"+dbName;
-		response.getWriter().append("Connect to : ").append(url);
+		response.setContentType("text/html;charset=UTF-8");
+		response.getWriter().append("<h2>Connect to : ").append(url).append("</h2>");
+		
+		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection  conn = DriverManager.getConnection(url, user, pass);
+			
+			String sql ="SELECT item_id,item_name,price FROM Items";
+			
+			PreparedStatement statement = conn.prepareStatement(sql);
+			
+			ResultSet rs = statement.executeQuery();
+			
+			while(rs.next()==true) {
+				response.getWriter().append("<h3>item_name:").append(rs.getString("item_name")).append("</h3>");
+				response.getWriter().append("<h4>item_id:").append(rs.getString("item_id")).append("</h4>");
+				response.getWriter().append("<h4>price:").append(rs.getString("price")).append("</h4>");
+
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
